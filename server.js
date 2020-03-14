a3d44dea7f124967db76f74e645df61ae4e1d29b
tauan-longaretti <tauanlongaretti@gmail.com>
2020-03-13 23:51:01 -0400

    get implememnted successfully in server file

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require("express");
 
+const db = require('./data/db.js');
+
 const dbRouter = require("./data/db-router.js");
 
 const server = express();
 
 server.use(express.json());
 
-server.use("/api/hubs", dbRouter);
+server.use("/api/posts", dbRouter);
 
 server.get('/', (req, res) => {
-    res.send(`
-        <h2>Lambda Hubs API</h>
-        <p>Welcome to the Lambda Hubs API</p>
-      `);
+    console.log(req.query);
+    db.find(req.query)
+        .then(posts => {
+          res.status(200).json(posts);
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ message: "Error retrieving the posts" });
+        });
   });
 
 module.exports = server;
