9bd05840ca241ce60b79def7c0084664f08b6587
tauan-longaretti <tauanlongaretti@gmail.com>
2020-03-13 23:54:11 -0400

    get transfered to db router file

--- data/db-router.js
+++ data/db-router.js
@@ -1,6 +1,20 @@
 const express = require('express');
+
 const db = require('./db.js');
 
 const router = express.Router();
+
+// GET - All posts
+router.get('/', (req, res) => {
+    console.log(req.query);
+    db.find(req.query)
+        .then(posts => {
+          res.status(200).json(posts);
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ message: "Error retrieving the posts" });
+        });
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 
-const db = require('./data/db.js');
-
 const dbRouter = require("./data/db-router.js");
 
 const server = express();
@@ -11,15 +9,10 @@ server.use(express.json());
 server.use("/api/posts", dbRouter);
 
 server.get('/', (req, res) => {
-    console.log(req.query);
-    db.find(req.query)
-        .then(posts => {
-          res.status(200).json(posts);
-        })
-        .catch(error => {
-          console.log(error);
-          res.status(500).json({ message: "Error retrieving the posts" });
-        });
+    res.send(`
+        <h2>Lambda Blog API</h>
+        <p>Welcome to the Lambda Blog API</p>
+      `);
   });
 
 module.exports = server;
